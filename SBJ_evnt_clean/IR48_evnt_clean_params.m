7cab53fdb3a9d86262c8b94de81896ea8a5e8e61
hoycw <hoycw@berkeley.edu>
2018-07-17 16:37:26 -0700

    IR48 event params

--- SBJ_evnt_clean/IR48_evnt_clean_params.m
+++ SBJ_evnt_clean/IR48_evnt_clean_params.m
@@ -1,16 +1,14 @@
-%% Photodiode Trace Cleaning Parameters: IR41
+%% Photodiode Trace Cleaning Parameters: IR48
 % Mark trials to ignore e.g., interruptions
 ignore_trials = [];
 
 % Set zero/baseline during a block
-bsln_val = 6500;
+bsln_val = 2520;
 
 % Record epochs (in sec) with fluctuations that should be set to baseline
-bsln_times = {...% The photodiode data between Block 1 and Block 2 is bad
-    [0.0 21.0],...% initial offset
-    [122.0 140.0],...% extra trial at end of B1
-    [240.0 280.0],...% fixes a dip in baseline b/w B2 and B3
-    [606.0 616.0]...% fixes a dip in baseline b/w B5 and B6
+bsln_times = {...
+    [0.0 10.0],...% initial offset
+    [109.0 111.0]...% extra trial at end of B1
     };
 % Record epochs (in sec) when baseline has shifted
 bsln_shift_times = {};
