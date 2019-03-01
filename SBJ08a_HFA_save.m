5dae0a25fbeeb5b09a0361d92d896d9912124a0c
hoycw <hoycw@berkeley.edu>
2019-02-28 16:48:00 -0800

    SBJ08a update to use filter-hilbert; all supplemental functions for that

--- SBJ08a_HFA_save.m
+++ SBJ08a_HFA_save.m
@@ -1,16 +1,19 @@
 function SBJ08a_HFA_save(SBJ,pipeline_id,an_id)
 % Calculates high frequency activity, computes cluster-based statistics, and plots the results
 % clear all; %close all;
+
 % Set up paths
-addpath('/home/knight/hoycw/PRJ_Stroop/scripts/');
-addpath('/home/knight/hoycw/PRJ_Stroop/scripts/utils/');
-addpath('/home/knight/hoycw/Apps/fieldtrip/');
+if exist('/home/knight/hoycw/','dir');root_dir='/home/knight/hoycw/';ft_dir=[root_dir 'Apps/fieldtrip/'];
+else root_dir='/Volumes/hoycw_clust/';ft_dir='/Users/colinhoy/Code/Apps/fieldtrip/';end
+addpath([root_dir 'PRJ_Stroop/scripts/']);
+addpath([root_dir 'PRJ_Stroop/scripts/utils/']);
+addpath(ft_dir);
 ft_defaults
 
 %% Data Preparation
-SBJ_vars_cmd = ['run /home/knight/hoycw/PRJ_Stroop/scripts/SBJ_vars/' SBJ '_vars.m'];
+SBJ_vars_cmd = ['run ' root_dir 'PRJ_Stroop/scripts/SBJ_vars/' SBJ '_vars.m'];
 eval(SBJ_vars_cmd);
-an_vars_cmd = ['run /home/knight/hoycw/PRJ_Stroop/scripts/an_vars/' an_id '_vars.m'];
+an_vars_cmd = ['run ' root_dir 'PRJ_Stroop/scripts/an_vars/' an_id '_vars.m'];
 eval(an_vars_cmd);
 % eval(['run /home/knight/hoycw/PRJ_Stroop/scripts/proc_vars/' pipeline_id '_proc_vars.m']);
 
@@ -22,23 +25,36 @@ load(strcat(SBJ_vars.dirs.events,SBJ,'_trial_info_final.mat'));
 cfgs = [];
 cfgs.channel = SBJ_vars.ch_lab.ROI;
 roi = ft_selectdata(cfgs,data);
+roi_fsample = roi.fsample;
+clear data;
 
 %% Cut into Trials
-% Pad trial_lim_s by cfg_hfa.t_ftimwin/2 to avoid NaNs in epoch of interest
-% Add 10 ms just because trimming back down to trial_lim_s exactly leave
-% one NaN on the end, so smoothing will NaN out everything
-
+% Padding:
+%   At a minimum, trial_lim_s must extend 1/2*max(filter_window) prior to 
+%   the first data point to be estimated to avoid edges in the filtering window.
+%   (ft_freqanalysis will return NaN for partially empty windows, e.g. an edge pre-trial,
+%   but ft_preprocessing would return a filtered time series with an edge artifact.)
+%   Also note this padding buffer should be at least 3x the slowest cycle
+%   of interest.
+if strcmp(HFA_type,'multiband')
+    pad_len = 0.5*max(cfg_hfa.t_ftimwin)*3;
+elseif any(strcmp(HFA_type,{'broadband','hilbert'}))
+    % add 250 ms as a rule of thumb, or onger if necessary
+    pad_len = 0.5*max([1/min(fois)*3 0.25]);
+end
+% Add extra 10 ms just because trimming back down to trial_lim_s exactly leave
+%   one NaN on the end (smoothing that will NaN out everything)
+trial_lim_s_pad = [trial_lim_s(1)-pad_len trial_lim_s(2)+pad_len+0.01];
 %!!! BEWARE: This is suboptimal for R-locked because trial_lim_s(1)=-0.5,
 %which adds 250ms to the time series that isn't necessary; the realign_tfr
 %function should still cut to the desired data, but it'll take longer.
-trial_lim_s_pad = [trial_lim_s(1)-max(cfg_hfa.t_ftimwin)/2 trial_lim_s(2)+max(cfg_hfa.t_ftimwin)/2+0.01];
 
 % Always normalize to pre-stimulus baseline for HFA
 bsln_events = trial_info.word_onset;
 if strcmp(event_type,'stim')
     % Cut to desired trial_lim_s
     roi_trl = fn_ft_cut_trials_equal_len(roi,bsln_events,trial_info.condition_n',...
-        round(trial_lim_s_pad*roi.fsample));
+        round(trial_lim_s_pad*roi_fsample));
 elseif strcmp(event_type,'resp')
     % Check that baseline will be included in trial_lim_s
     if trial_lim_s(1)>bsln_lim(1)
@@ -47,10 +63,11 @@ elseif strcmp(event_type,'resp')
     % Cut out to max_RT+trial_lim_s(2)+max(cfg_hfa.t_ftimwin)
     max_RT  = max(trial_info.response_time);
     roi_trl = fn_ft_cut_trials_equal_len(roi,bsln_events,trial_info.condition_n',...
-        round([trial_lim_s_pad(1) max_RT+trial_lim_s_pad(2)]*roi.fsample));
+        round([trial_lim_s_pad(1) max_RT+trial_lim_s_pad(2)]*roi_fsample));
 else
     error(['Unknown event_type: ' event_type]);
 end
+clear roi;
 
 %% Compute HFA
 fprintf('===================================================\n');
@@ -59,32 +76,36 @@ fprintf('===================================================\n');
 if strcmp(HFA_type,'multiband')
     cfg_hfa.trials = 'all';
     hfa = ft_freqanalysis(cfg_hfa, roi_trl);
+elseif strcmp(HFA_type,'hilbert')
+    % Hilbert method to extract power
+    hfas = cell(size(fois));
+    orig_lab = roi_trl.label;
+    for f_ix = 1:numel(fois)
+        cfg_hfa.bpfreq = bp_lim(f_ix,:);
+        fprintf('\n------> %s filtering: %.03f - %.03f\n', HFA_type, bp_lim(f_ix,1), bp_lim(f_ix,2));
+        hfas{f_ix} = ft_preprocessing(cfg_hfa,roi_trl);
+        hfas{f_ix}.label = strcat(hfas{f_ix}.label,[':' num2str(fois(f_ix),4)]);
+    end
+    % Treat different freqs as channels
+    hfa_tmp = hfas{1};      % Save to plug in averaged data
+    hfa = ft_appenddata([], hfas{:}); clear hfas;
 elseif strcmp(HFA_type,'broadband')
-    error('Stop using broadband and use multitapers you dummy!');
-    %         % Filter to HFA band
-    %         cfgpp = [];
-    %         cfgpp.hpfilter  = 'yes';
-    %         cfgpp.hpfreq    = 70;
-    %         cfgpp.lpfilter  = 'yes';
-    %         cfgpp.lpfreq    = 150;
+    error('Stop using broadband and use filter-hilbert or multitapers you dummy!');
+    %         % Filter to single HFA band
+    %         cfgpp=[];.hpfilter='yes';.hpfreq=70;.lpfilter='yes';lpfreq=150;
     %         roi = ft_preprocessing(cfgpp,roi);
-    %         % Hilbert method to extract power
 else
     error('Unknown HFA_type provided');
 end
 
 % Trim back down to original trial_lim_s to exclude NaNs
-if strcmp(event_type,'stim')
-    cfg_trim = [];
-    cfg_trim.latency = trial_lim_s;
-    hfa = ft_selectdata(cfg_trim,hfa);
-elseif strcmp(event_type,'resp')
-    cfg_trim = [];
-    cfg_trim.latency = [trial_lim_s(1) max_RT+trial_lim_s(2)];
-    hfa = ft_selectdata(cfg_trim,hfa);
-else
-    error(['Unknown event_type: ' event_type]);
+cfg_trim = [];
+cfg_trim.latency = trial_lim_s;
+if strcmp(event_type,'resp')
+    cfg_trim.latency(2) = max_RT+trial_lim_s(2);
 end
+hfa = ft_selectdata(cfg_trim,hfa);
+hfa_tmp = ft_selectdata(cfg_trim,hfa_tmp);
 
 %% Baseline Correction
 fprintf('===================================================\n');
@@ -92,7 +113,13 @@ fprintf('---------------- Baseline Correction --------------\n');
 fprintf('===================================================\n');
 switch bsln_type
     case {'zboot', 'zscore'}
-        hfa = fn_bsln_ft_tfr(hfa,bsln_lim,bsln_type,n_boots);
+        if strcmp(HFA_type,'multiband')
+            hfa = fn_bsln_ft_tfr(hfa,bsln_lim,bsln_type,n_boots);
+        elseif any(strcmp(HFA_type,{'broadband','hilbert'}))
+            hfa = fn_bsln_ft_filtered(hfa,bsln_lim,bsln_type,n_boots);
+        else
+            error('Unknown HFA_type provided');
+        end
     case {'relchange', 'demean', 'my_relchange'}
         error(['bsln_type ' bsln_type ' is not compatible with one-sample t test bsln activation stats']);
 %         cfgbsln = [];
@@ -106,22 +133,38 @@ end
 
 %% Smooth Power Time Series
 if smooth_pow_ts
+    % error catches
+    if ~strcmp(lp_yn,'yes')
+        if strcmp(hp_yn,'yes')
+            error('Why are you only high passing?');
+        else
+            error('Why is smooth_pow_ts yes but no lp or hp?');
+        end
+    end
     fprintf('===================================================\n');
     fprintf('----------------- Filtering Power -----------------\n');
     fprintf('===================================================\n');
-    for ch_ix = 1:numel(hfa.label)
-        for f_ix = 1:numel(hfa.freq)
-            if strcmp(lp_yn,'yes') && strcmp(hp_yn,'yes')
-                hfa.powspctrm(:,ch_ix,f_ix,:) = fn_EEGlab_bandpass(...
-                    hfa.powspctrm(:,ch_ix,f_ix,:), roi.fsample, hp_freq, lp_freq);
-            elseif strcmp(lp_yn,'yes')
-                hfa.powspctrm(:,ch_ix,f_ix,:) = fn_EEGlab_lowpass(...
-                    squeeze(hfa.powspctrm(:,ch_ix,f_ix,:)), roi.fsample, lp_freq);
-            elseif strcmp(hp_yn,'yes')
-                error('Why are you only high passing?');
-            else
-                error('Why did you say yes smooth but no to both low and high pass?');
+    if isfield(hfa,'powspctrm')
+        for ch_ix = 1:numel(hfa.label)
+            for f_ix = 1:numel(hfa.freq)
+                if strcmp(lp_yn,'yes') && strcmp(hp_yn,'yes')
+                    hfa.powspctrm(:,ch_ix,f_ix,:) = fn_EEGlab_bandpass(...
+                        hfa.powspctrm(:,ch_ix,f_ix,:), roi_fsample, hp_freq, lp_freq);
+                elseif strcmp(lp_yn,'yes')
+                    hfa.powspctrm(:,ch_ix,f_ix,:) = fn_EEGlab_lowpass(...
+                        hfa.powspctrm(:,ch_ix,f_ix,:), roi_fsample, lp_freq);
+                else
+                    error('weird non-Y/N filtering options!');
+                end
             end
+        end
+    else
+        if strcmp(lp_yn,'yes') && strcmp(hp_yn,'yes')
+            hfa.trial{1} = fn_EEGlab_bandpass(hfa.trial{1}, roi_fsample, hp_freq, lp_freq);
+        elseif strcmp(lp_yn,'yes')
+            hfa.trial{1} = fn_EEGlab_lowpass(hfa.trial{1}, roi_fsample, lp_freq);
+        else
+            error('weird non-Y/N filtering options!');
         end
     end
 end
@@ -132,17 +175,42 @@ if strcmp(HFA_type,'multiband')
     cfg_avg.freq = 'all';
     cfg_avg.avgoverfreq = 'yes';
     hfa = ft_selectdata(cfg_avg,hfa);
+elseif strcmp(HFA_type,'hilbert')
+    hfa_tmp.label = orig_lab;
+    lab_ix = 1;
+    ch_check = zeros(size(hfa.label));
+    for ch_ix = 1:numel(hfa_tmp.label)
+        ch_lab_ix = find(~cellfun(@isempty,strfind(hfa.label,hfa_tmp.label{ch_ix})));
+        ch_check(ch_lab_ix) = ch_check(ch_lab_ix)+ch_ix;
+        for t_ix = 1:numel(hfa_tmp.trial)
+            hfa_tmp.trial{t_ix}(ch_ix,:) = mean(hfa.trial{t_ix}(ch_lab_ix,:),1);
+        end
+        lab_ix = ch_lab_ix(end)+1;
+    end
+    hfa = hfa_tmp; clear hfa_tmp;
 end
 
 %% Re-align to event of interest if necessary (e.g., response)
 if strcmp(event_type,'resp')
-    hfa = fn_realign_tfr_s2r(hfa,trial_info.response_time,trial_lim_s);
+    if strcmp(HFA_type,'multiband')
+        hfa = fn_realign_tfr_s2r(hfa,trial_info.response_time,trial_lim_s);
+    elseif any(strcmp(HFA_type,{'broadband','hilbert'}))
+        hfa = fn_realign_filt_s2r(hfa,trial_info.response_time,trial_lim_s);
+    end
 elseif ~strcmp(event_type,'stim')
     error(['ERROR: unknown event_type ' event_type]);
 end
 
+%% Downsample
+if resample_ts && hfa.fsample~=resample_freq
+    cfgrs = [];
+    cfgrs.resamplefs = resample_freq;
+    cfgrs.detrend = 'no';
+    hfa = ft_resampledata(cfgrs, hfa);
+end
+
 %% Save Results
-data_out_filename = strcat(SBJ_vars.dirs.proc,SBJ,'_HFA_ROI_',an_id,'.mat');
+data_out_filename = strcat(SBJ_vars.dirs.proc,SBJ,'_ROI_',an_id,'.mat');
 fprintf('===================================================\n');
 fprintf('--- Saving %s ------------------\n',data_out_filename);
 fprintf('===================================================\n');
---
+++ utils/fn_realign_filt_s2r.m
@@ -0,0 +1,37 @@
+function [filt_r] = fn_realign_filt_s2r(filt_s,rt,trial_lim_sec)
+%% Realign a stimulus-locked TFR to the responses indicated by offsets
+% INPUTS:
+%   filt_s [ft struct] - output of ft_preprocessing to be realigned
+%   rt [int array] - n_trialsx1 array of RTs (TIMES in SEC) within that trial
+%   trial_lim [int, int] - [start, end] array of TIME (in SEC) indices of epoch around RT
+% OUTPUTS:
+%   filt_r [ft_struct] - realigned tfr with correct .trial and .time fields
+
+% Check failure cases
+if ~isa(filt_s.time{1},'double')
+    error('tfr_s.time field is not a double. Trials are probably not the same length!');
+end
+for t = 1:numel(filt_s.time)
+    if sum(filt_s.time{t}==filt_s.time{1})~=numel(filt_s.time{1})
+        error('Not all trials are same length!');
+    end
+end
+
+% Compute output trial/time size
+[~, t1_beg_ix] = min(abs(filt_s.time{1}-(rt(1)+trial_lim_sec(1))));
+[~, t1_end_ix] = min(abs(filt_s.time{1}-(rt(1)+trial_lim_sec(2))));
+[~, t1_rt_ix]  = min(abs(filt_s.time{1}-rt(1)));
+new_time = filt_s.time{1}(t1_beg_ix:t1_end_ix)-filt_s.time{1}(t1_rt_ix);
+
+% Create new time field
+filt_r = filt_s;
+% orig_size = size(filt_s.powspctrm);
+% filt_r.powspctrm = NaN([orig_size(1) orig_size(2) orig_size(3) numel(filt_r.time)]);
+for t = 1:numel(filt_s.trial)
+    filt_r.time{t} = new_time;
+    [~, beg_ix] = min(abs(filt_s.time{t}-(rt(t)+trial_lim_sec(1))));
+    [~, end_ix] = min(abs(filt_s.time{t}-(rt(t)+trial_lim_sec(2))));
+    filt_r.trial{t} = filt_s.trial{1}(:,beg_ix:end_ix);
+end
+
+end
