c5451c6354f1349d7059471f69a76d9a9fdcb539
hoycw <hoycw@berkeley.edu>
2019-05-28 12:17:49 -0700

    SBJ10c errbr works with new st now; SGE opts for CNI_pcon on zbtS; new actv stat for baseline

--- SBJ10c_HFA_GRP_summary_errbar_perc_GMlim_actv_RT_ANOVA_ROI.m
+++ SBJ10c_HFA_GRP_summary_errbar_perc_GMlim_actv_RT_ANOVA_ROI.m
@@ -1,4 +1,4 @@
-function SBJ10c_HFA_GRP_summary_errbar_perc_GMlim_actv_RT_ANOVA_ROI(SBJs,stat_id,proc_id,an_id,actv_win,roi_id,...
+function SBJ10c_HFA_GRP_summary_errbar_perc_GMlim_actv_RT_ANOVA_ROI(SBJs,stat_id,proc_id,an_id,actv_id,roi_id,...
                                                             atlas_id,gm_thresh,plt_id,plot_out,plot_scat,save_fig,fig_vis,fig_ftype)
 % Load HFA analysis results for active, RT correlation, and ANOVA epochs
 %   RT correlation: any significance in stat_lim
@@ -11,7 +11,6 @@ label_spacer = 0;
 groi_label_spacer = '      ';
 if ischar(save_fig); save_fig = str2num(save_fig); end
 if ischar(plot_scat); plot_scat = str2num(plot_scat); end
-if isnumeric(actv_win); actv_win = num2str(actv_win); end
 
 %% Data Preparation
 % Set up paths
@@ -24,16 +23,19 @@ ft_defaults
 %% Prep variables
 an_vars_cmd = ['run ' root_dir 'PRJ_Stroop/scripts/an_vars/' an_id '_vars.m'];
 eval(an_vars_cmd);
-plt_vars_cmd = ['run ' root_dir 'PRJ_Stroop/scripts/plt_vars/' plt_id '_vars.m'];
-eval(plt_vars_cmd);
-stat_vars_cmd = ['run ' root_dir 'PRJ_Stroop/scripts/stat_vars/' stat_id '_vars.m'];
-eval(stat_vars_cmd);
+% plt_vars_cmd = ['run ' root_dir 'PRJ_Stroop/scripts/plt_vars/' plt_id '_vars.m'];
+% eval(plt_vars_cmd);
 
-% Get condition info
-[grp_lab, grp_colors, grp_style] = fn_group_label_styles(model_lab);
-% if rt_correlation
-[rt_lab, rt_color, rt_style]     = fn_group_label_styles('RT');
-% end
+% Get example condition info
+load([root_dir 'PRJ_Stroop/data/' SBJs{1} '/04_proc/' SBJs{1} '_mANOVA_ROI_' stat_id '_' an_id '.mat'],'st');
+evnt_lab = st.evnt_lab;
+[grp_lab, ~, ~] = fn_group_label_styles(st.model_lab);
+rt_grp = st.rt_corr;
+if rt_grp
+    stat_lab = [{'Active','Deactive','corr(RT)'},grp_lab];
+else
+    stat_lab = [{'Active','Deactive'},grp_lab];
+end
 
 % Load all ROI info
 [roi_list, roi_colors] = fn_roi_label_styles(roi_id);
@@ -42,11 +44,11 @@ eval(stat_vars_cmd);
 actv_cnt  = zeros([numel(SBJs) numel(roi_list)]);
 dact_cnt  = zeros([numel(SBJs) numel(roi_list)]);
 % cse_cnt  = zeros([numel(SBJs) numel(roi_list)]);
-rt_cnt    = zeros([numel(SBJs) numel(roi_list)]);
 grp_cnt   = zeros([numel(SBJs) numel(roi_list) numel(grp_lab)]);
 elec_cnt  = zeros([numel(SBJs) numel(roi_list)]);
+if rt_grp; rt_cnt = zeros([numel(SBJs) numel(roi_list)]); end
 
-mean_RTs = NaN(size(SBJs));
+if strcmp(evnt_lab,'S'); mean_RTs = NaN(size(SBJs)); end
 
 %% Load Results
 for sbj_ix = 1:numel(SBJs)
@@ -56,17 +58,15 @@ for sbj_ix = 1:numel(SBJs)
     SBJ_vars_cmd = ['run ' root_dir 'PRJ_Stroop/scripts/SBJ_vars/' SBJ '_vars.m'];
     eval(SBJ_vars_cmd);
     
-    % Compute mean RT
-    load(strcat(SBJ_vars.dirs.events,SBJ,'_trial_info_final.mat'),'trial_info');
-    if strcmp(event_type,'stim')
-        mean_RTs(sbj_ix) = mean(trial_info.response_time);
-    end
     % Load ANOVA
     load(strcat(SBJ_vars.dirs.proc,SBJ,'_mANOVA_ROI_',stat_id,'_',an_id,'.mat'));
+    if ~strcmp(st.evnt_lab,evnt_lab); error('mismatching evnt_lab in w2 stats'); end
     
     % Load actv stats
-    actv_fname = strcat(SBJ_vars.dirs.proc,SBJ,'_ROI_',an_id,'_actv_mn',actv_win,'.mat');
+    actv_fname = strcat(SBJ_vars.dirs.proc,SBJ,'_ROI_',an_id,'_',actv_id,'.mat');
     load(actv_fname,'actv_ch','actv_ch_epochs');
+    tmp = load(actv_fname,'st');
+    if ~strcmp(tmp.st.evnt_lab,evnt_lab); error('mismatching evnt_lab in actv stats'); end
     
     % Load HFA (to get sign of activation)
     hfa_fname = strcat(SBJ_vars.dirs.proc,SBJ,'_ROI_',an_id,'.mat');
@@ -74,6 +74,41 @@ for sbj_ix = 1:numel(SBJs)
     
 %     CSE_filename = strcat(SBJ_vars.dirs.proc,SBJ,'_ROI_',cse_an_id,'_',conditions,'.mat');%an_id
 %     tmp = load(CSE_filename,'stat'); cse = tmp.stat;
+    
+    % Compute mean RT
+    if strcmp(evnt_lab,'S')
+        load(strcat(SBJ_vars.dirs.events,SBJ,'_trial_info_final.mat'),'trial_info');
+        mean_RTs(sbj_ix) = mean(trial_info.response_time);
+    end
+    
+    %% Process parameters
+    %!!! is this the best way to do this??? Maybe not...
+    sample_rate = (numel(hfa.time)-1)/(hfa.time(end)-hfa.time(1));
+    
+    % Restrict hfa to stat_lim (should obviate some of below comparisons)
+    cfg_lim = [];
+    cfg_lim.latency = st.stat_lim;
+    hfa = ft_selectdata(cfg_lim,hfa);
+    if rt_grp; stat = ft_selectdata(cfg_lim,stat); end
+%     cse      = ft_selectdata(cfg_lim,cse);
+    
+    % Confirm channels and time axis are the same
+    if ~isempty(setdiff(w2.label,hfa.label));
+        error('Different electrodes across hfa analyses!');
+    end
+    if rt_grp
+        % All the rounding and such is because some stupid rounding errors...
+        same_start = round(hfa.time(1)*uint8(sample_rate))==round(stat.time(1)*uint8(sample_rate));
+        same_end   = round(hfa.time(end)*uint8(sample_rate))==round(stat.time(end)*uint8(sample_rate));
+        same_numel = size(hfa.time,2)==size(stat.time,2);
+        if ~same_start || ~same_end || ~same_numel
+            error('time axes are not the same across hfa analyses!');
+        end
+        if ~isempty(setdiff(stat.label,hfa.label));
+            error('Different electrodes across hfa analyses!');
+        end
+    end
+    
     %% Load ROI and GM/WM info
     if strcmp(atlas_id,'Yeo7')
         elec_fname = [SBJ_vars.dirs.recon SBJ '_elec_' proc_id '_mni_v_' atlas_id '.mat'];
@@ -82,8 +117,9 @@ for sbj_ix = 1:numel(SBJs)
     end
     load(elec_fname);
     
-    % Sort elecs by stat labels
-    cfgs = []; cfgs.channel = stat.label;
+    %!!! eventually load this first as my ground truth for elec_cnt
+    % Sort elecs by hfa labels
+    cfgs = []; cfgs.channel = hfa.label;
     elec = fn_select_elec(cfgs,elec);
     elec.roi = fn_atlas2roi_labels(elec.atlas_lab,atlas_id,roi_id);
     
@@ -91,8 +127,9 @@ for sbj_ix = 1:numel(SBJs)
     if ~plot_out
         cfgs = []; cfgs.channel = fn_select_elec_lab_match(elec, 'b', atlas_id, roi_id);
         elec = fn_select_elec(cfgs, elec);
-        stat = ft_selectdata(cfgs,stat);
+        hfa = ft_selectdata(cfgs,hfa);
         w2 = ft_selectdata(cfgs,w2);
+        if rt_grp; stat = ft_selectdata(cfgs,stat); end
     end
     
     % Get GM probability from tissue labels {'GM','WM','CSF','OUT'}
@@ -102,37 +139,8 @@ for sbj_ix = 1:numel(SBJs)
         gm_bin = ones(size(elec.label));
     end
     
-    %% Process parameters
-    %!!! is this the best way to do this??? Maybe not...
-    sample_rate = (numel(stat.time)-1)/(stat.time(end)-stat.time(1));
-    
-    % Restrict hfa to stat_lim (should obviate some of below comparisons)
-    cfg_lim = [];
-    cfg_lim.latency = stat_lim;
-    hfa      = ft_selectdata(cfg_lim,hfa);
-    stat     = ft_selectdata(cfg_lim,stat);
-%     cse      = ft_selectdata(cfg_lim,cse);
-    
-    % FDR correct pvalues for ANOVA
-    qvals = NaN(size(w2.pval));
-    for ch_ix = 1:numel(stat.label)
-        [~, ~, ~, qvals(:,ch_ix,:)] = fdr_bh(squeeze(w2.pval(:,ch_ix,:)));%,0.05,'pdep','yes');
-    end
-    
-    % Confirm channels and time axis are the same
-    %   All the rounding and such is because some stupid rounding errors...
-    same_start = round(hfa.time(1)*uint8(sample_rate))==round(stat.time(1)*uint8(sample_rate));
-    same_end   = round(hfa.time(end)*uint8(sample_rate))==round(stat.time(end)*uint8(sample_rate));
-    same_numel = size(hfa.time,2)==size(stat.time,2);
-    if ~same_start || ~same_end || ~same_numel
-        error('time axes are not the same across hfa analyses!');
-    end
-    if ~isempty(setdiff(stat.label,hfa.label));
-        error('Different electrodes across hfa analyses!');
-    end
-    
     %% Aggregate results per ROI
-    for ch_ix = 1:numel(stat.label)
+    for ch_ix = 1:numel(hfa.label)
         % If elec matches roi_list, get stats
         if any(strcmp(elec.roi{ch_ix},roi_list)) && gm_bin(ch_ix)
             roi_ix = find(strcmp(elec.roi{ch_ix},roi_list));
@@ -141,12 +149,12 @@ for sbj_ix = 1:numel(SBJs)
 %             if any(cse.mask(ch_ix,1,:))
 %                 cse_cnt(sbj_ix,roi_ix) = cse_cnt(sbj_ix,roi_ix)+1;
             % Check if active, get epochs
-            if any(strcmp(stat.label{ch_ix},actv_ch))
+            if any(strcmp(hfa.label{ch_ix},actv_ch))
                 % Find significant epoch indices
-                actv_epochs = actv_ch_epochs{strcmp(stat.label{ch_ix},actv_ch)};
+                actv_epochs = actv_ch_epochs{strcmp(hfa.label{ch_ix},actv_ch)};
                 
                 % Toss late epochs if S-locked
-                if strcmp('stim',event_type)
+                if strcmp(evnt_lab,'S')
                     actv_epochs(actv_epochs(:,1)>mean_RTs(sbj_ix),:) = [];
                 end
                 
@@ -174,35 +182,19 @@ for sbj_ix = 1:numel(SBJs)
             end
             
             % Check for RT correlations, get epochs
-            if sum(squeeze(stat.mask(ch_ix,1,:)))>0
-%                 mask_chunks = fn_find_chunks(squeeze(stat.mask(ch_ix,1,:)));
-%                 mask_chunks(squeeze(stat.mask(ch_ix,1,mask_chunks(:,1)))==0,:) = [];
-%                 % Convert to time
-%                 for ep_ix = 1:size(mask_chunks,1)
-%                     mask_chunks(ep_ix,1) = stat.time(mask_chunks(ep_ix,1));
-%                     mask_chunks(ep_ix,2) = stat.time(mask_chunks(ep_ix,2));
-%                 end
-%                 %             cond_count(roi_ix) = cond_count(roi_ix) + 1;
-%                 if strcmp('stim',event_type)
-%                     % Exclude differences after the mean RT for this SBJ
-%                     if min(mask_chunks(:,1)) < mean_RTs(sbj_ix)
-%                         cond_g_count(sbj_ix,groi_ix) = cond_g_count(sbj_ix,groi_ix) + 1;
-%                         both_ix = both_ix + 1;
-%                     end
-%                 else
+            if rt_grp && sum(squeeze(stat.mask(ch_ix,1,:)))>0
                 rt_cnt(sbj_ix,roi_ix) = rt_cnt(sbj_ix,roi_ix) + 1;
-%                 end
             end
             
             % Check for ANOVA group effects
             for grp_ix = 1:numel(grp_lab)
-                if any(squeeze(qvals(grp_ix,ch_ix,:))<0.05)
+                if any(squeeze(w2.qval(grp_ix,ch_ix,:))<st.alpha)
                     grp_cnt(sbj_ix,roi_ix,grp_ix) = grp_cnt(sbj_ix,roi_ix,grp_ix)+1;
                 end
             end
         end
     end
-    clear SBJ SBJ_vars w2 stat elec hfa actv_ch actv_ch_epochs tmp trial_info qvals %cse
+    clear SBJ SBJ_vars w2 stat elec hfa actv_ch actv_ch_epochs tmp trial_info qvals st %cse
 end
 
 %% Plot Percentage of Electrodes Active, Deactive, and Condition Sensitive
@@ -212,7 +204,7 @@ else
     scat_suffix = '';
 end
 % Create and format the plot
-fig_name = ['GRP_HFA_errbar_perc_GMlim' num2str(gm_thresh*100) '_actv_' stat_id '_' roi_id '_' event_type scat_suffix];
+fig_name = ['GRP_HFA_errbar_perc_GMlim' num2str(gm_thresh*100) '_' stat_id '_' actv_id '_' roi_id '_' evnt_lab scat_suffix];
 figure('Name',fig_name,'units','normalized',...
         'outerposition',[0 0 0.5 0.6],'Visible',fig_vis);
 hold on;
@@ -224,9 +216,10 @@ hold on;
 % delete(h2);
 
 % Compile Data in Plotting Format
-scat_vars = {actv_cnt, dact_cnt, rt_cnt};%cse_cnt, 
-bar_data  = zeros([3+numel(grp_lab) numel(roi_list)]);%+1 CSE
-var_data  = zeros([3+numel(grp_lab) numel(roi_list)]);%+1 CSE
+scat_vars = {actv_cnt, dact_cnt};%cse_cnt, 
+if rt_grp; scat_vars = [scat_vars {rt_cnt}]; end
+bar_data  = zeros([numel(stat_lab) numel(roi_list)]);%+1 CSE
+var_data  = zeros([numel(stat_lab) numel(roi_list)]);%+1 CSE
 for roi_ix = 1:numel(roi_list)
 %     bar_vars(1,roi_ix) = sum(cI_cnt(:,roi_ix))/sum(elec_cnt(:,roi_ix));
 %     bar_data(1,roi_ix) = sum(cse_cnt(:,roi_ix))/sum(elec_cnt(:,roi_ix));
@@ -235,12 +228,14 @@ for roi_ix = 1:numel(roi_list)
     var_data(1,roi_ix) = nanstd(actv_cnt(:,roi_ix)./elec_cnt(:,roi_ix))/sqrt(numel(SBJs));
     bar_data(2,roi_ix) = sum(dact_cnt(:,roi_ix))/sum(elec_cnt(:,roi_ix));
     var_data(2,roi_ix) = nanstd(dact_cnt(:,roi_ix)./elec_cnt(:,roi_ix))/sqrt(numel(SBJs));
-    bar_data(3,roi_ix) = sum(rt_cnt(:,roi_ix))/sum(elec_cnt(:,roi_ix));
-    var_data(3,roi_ix) = nanstd(rt_cnt(:,roi_ix)./elec_cnt(:,roi_ix))/sqrt(numel(SBJs));
+    if rt_grp
+        bar_data(3,roi_ix) = sum(rt_cnt(:,roi_ix))/sum(elec_cnt(:,roi_ix));
+        var_data(3,roi_ix) = nanstd(rt_cnt(:,roi_ix)./elec_cnt(:,roi_ix))/sqrt(numel(SBJs));
+    end
     for grp_ix = 1:numel(grp_lab)
-        bar_data(3+grp_ix,roi_ix) = sum(grp_cnt(:,roi_ix,grp_ix))/sum(elec_cnt(:,roi_ix));
-        var_data(3+grp_ix,roi_ix) = nanstd(grp_cnt(:,roi_ix,grp_ix)./elec_cnt(:,roi_ix))/sqrt(numel(SBJs));
-        scat_vars{3+grp_ix} = squeeze(grp_cnt(:,:,grp_ix));
+        bar_data(2+rt_grp+grp_ix,roi_ix) = sum(grp_cnt(:,roi_ix,grp_ix))/sum(elec_cnt(:,roi_ix));
+        var_data(2+rt_grp+grp_ix,roi_ix) = nanstd(grp_cnt(:,roi_ix,grp_ix)./elec_cnt(:,roi_ix))/sqrt(numel(SBJs));
+        scat_vars{2+rt_grp+grp_ix} = squeeze(grp_cnt(:,:,grp_ix));
     end
 end
 
@@ -248,17 +243,17 @@ end
 b = {};
 scat_offsets = linspace(-0.015,0.015,numel(SBJs));
 bar_offsets  = linspace(-0.25,0.25,numel(roi_list));   %bar for activation, deactivation, condition
-for cond_ix = 1:3+numel(grp_lab)
+for cond_ix = 1:2+rt_grp+numel(grp_lab)
     % Use "stacked" bars that have empty elements to trick MATLAB into
     % thinking there are multiple elements, which lets me change properties of individual bars
     b{cond_ix} = bar(bar_offsets+cond_ix,diag(bar_data(cond_ix,:)),0.9,'stacked');
     for roi_ix = 1:numel(roi_list)
         set(b{cond_ix}(roi_ix),'FaceColor',roi_colors{roi_ix},'EdgeColor','k');
-        % Plot individual subject percentages as scatters on top
+        % Add error bar across SBJs
         line([bar_offsets(roi_ix) bar_offsets(roi_ix)]+cond_ix,...
             [bar_data(cond_ix,roi_ix)+var_data(cond_ix,roi_ix) bar_data(cond_ix,roi_ix)-var_data(cond_ix,roi_ix)],...
             'Color','k','LineWidth',1.5);
-        % Overlay scatter for individual SBJ
+        % Plot individual subject percentages as scatters on top
         if plot_scat
             has_elecs = squeeze(elec_cnt(:,roi_ix)~=0);
             s = scatter(scat_offsets(has_elecs)+bar_offsets(roi_ix)+cond_ix,...
@@ -266,12 +261,12 @@ for cond_ix = 1:3+numel(grp_lab)
         end
     end
 end
-% if strcmp(event_type,'stim')
+% if strcmp(st.evnt_lab,'S')
 %     leg_loc = 'northwest';
 % else
 %     leg_loc = 'northeast';
 % end
-leg_loc = 'northeast';
+leg_loc = 'best';%'northeast';
 if plot_scat
     legend([b{1},s],roi_list{:},'Individuals','Location',leg_loc);
 else
@@ -282,10 +277,10 @@ end
 ax = gca;
 % ax.XLabel.String   = 'Time (s)';
 % ax.XLabel.FontSize = 14;
-ax.XLim    = [0.5 3.5+numel(grp_lab)];
-ax.XTick   = 1:3+numel(grp_lab);
+ax.XLim    = [0.5 0.5+numel(stat_lab)];
+ax.XTick   = 1:numel(stat_lab);
 ax.XColor  = 'k';
-ax.XTickLabel = {'Activation','Deactivation','Corr(RT)',grp_lab{:}};%'CSE'
+ax.XTickLabel = stat_lab;%'CSE'
 
 ax.YLabel.String   = 'Proportion of Electrodes';
 ax.YLabel.FontSize = 14;
@@ -302,15 +297,15 @@ ax.Title.FontSize = 16;
 
 %% Save figure
 if save_fig
-    fig_dir = [root_dir 'PRJ_Stroop/results/HFA/GRP_summary_errbar_ROI/actv_'...
-        stat_id '_' roi_id '/' an_id '_mn' actv_win '/'];
+    fig_dir = [root_dir 'PRJ_Stroop/results/HFA/GRP_summary_errbar_ROI/' ...
+        stat_id '-' actv_id '/' an_id '/' roi_id '/'];
     if ~exist(fig_dir,'dir')
         [~,~] = mkdir(fig_dir);
     end
     
-    fig_filename = [fig_dir fig_name '.' fig_ftype];
-    fprintf('Saving %s\n',fig_filename);
-    saveas(gcf,fig_filename);
+    fig_fname = [fig_dir fig_name '.' fig_ftype];
+    fprintf('Saving %s\n',fig_fname);
+    saveas(gcf,fig_fname);
 end
 
 end
---
+++ stat_vars/actv_S2t0_vars.m
@@ -0,0 +1,7 @@
+% Parameters for HFA actvation vs. baseline
+st.model_lab = 'actv';
+st.groups    = {'actv'};    % for compatibility
+st.evnt_lab = 'S';
+st.stat_lim = [-0.2 0];
+st.actv_win = 0;  % minimum window to be different than baseline
+st.alpha    = 0.05;
