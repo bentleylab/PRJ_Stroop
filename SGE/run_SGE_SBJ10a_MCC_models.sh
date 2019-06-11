6822514d40161ee4112742dc34961e097b868128
hoycw <hoycw@berkeley.edu>
2019-06-11 13:07:22 -0700

    run.sh script for SGE models

---
+++ SGE/run_SGE_SBJ10a_MCC_models.sh
@@ -0,0 +1,10 @@
+submit -s calls/SBJ10a_crRT_smANOVA.sh -f SBJ_lists/CCN2019_abstract_list.sbj -o opts/SBJ10a_PC_B2t0_HGm_S2t151_zbtA_sm0_wn100.opts
+
+submit -s calls/SBJ10a_crRT_smANOVA.sh -f SBJ_lists/CCN2019_abstract_list.sbj -o opts/SBJ10a_CNI_PC_S0tmRT_WL1_WS50_HGm_S2t151_zbtA_sm0_wn100.opts 
+submit -s calls/SBJ10a_crRT_smANOVA.sh -f SBJ_lists/CCN2019_abstract_list.sbj -o opts/SBJ10a_CNI_PC_D1tRT_HGmS2t251_zbtA_sm0_wn100.opts
+submit -s calls/SBJ10a_crRT_smANOVA.sh -f SBJ_lists/CCN2019_abstract_list.sbj -o opts/SBJ10a_CNI_PC_R1t5_WL1_WS50_HGm_R5t101_zbtA_sm0_wn100.opts 
+
+submit -s calls/SBJ10a_crRT_smANOVA.sh -f SBJ_lists/CCN2019_abstract_list.sbj -o opts/SBJ10a_PCi_S0tmRT_WL1_WS50_HGm_S2t151_zbtA_sm0_wn100.opts 
+submit -s calls/SBJ10a_crRT_smANOVA.sh -f SBJ_lists/CCN2019_abstract_list.sbj -o opts/SBJ10a_PCi_D1tRT_HGmS2t251_zbtA_sm0_wn100.opts 
+submit -s calls/SBJ10a_crRT_smANOVA.sh -f SBJ_lists/CCN2019_abstract_list.sbj -o opts/SBJ10a_PCi_R1t5_WL1_WS50_HGm_R5t101_zbtA_sm0_wn100.opts 
+
