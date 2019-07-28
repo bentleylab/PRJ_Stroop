f19dec5681959123d2d6255a1951bbd4609f583b
hoycw <hoycw@berkeley.edu>
2019-07-27 17:42:53 -0700

    one time to mv onld elec files

---
+++ one_time_scripts/mv_elec_old_pipeline_07.27.2019.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Move to scripts directory 
+root_dir="/home/knight/hoycw/PRJ_Stroop/"
+cd $root_dir/scripts/
+
+declare -a SBJs
+SBJs=(`cat "${root_dir}scripts/SGE/SBJ_lists/MCC2019_poster_list.sbj"`)
+
+# Run BHV scripts for these SBJs
+for sbj in "${SBJs[@]}"; do
+    cd ${root_dir}/data/${sbj}/05_recon/
+    mkdir old_pipeline/
+    mv *full* old_pipeline/
+    mv *gROI* old_pipeline/
+    mv *compiled* old_pipeline/
+done
+
+
