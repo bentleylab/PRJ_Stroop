1908cdbdf2f978b59f47d9055dc481fad6f60320
hoycw <hoycw@berkeley.edu>
2019-07-27 17:58:43 -0700

    mv.sh script for elec*tis*

--- one_time_scripts/mv_elec_old_pipeline_07.27.2019.sh
+++ one_time_scripts/mv_elec_old_pipeline_07.27.2019.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Move to scripts directory 
-root_dir="/Volumes/hoycw_clust/PRJ_Stroop/"
+root_dir="/home/knight/hoycw/PRJ_Stroop/"
 cd $root_dir/scripts/
 
 declare -a SBJs
@@ -10,10 +10,11 @@ SBJs=(`cat "${root_dir}scripts/SGE/SBJ_lists/MCC2019_poster_list.sbj"`)
 # Run BHV scripts for these SBJs
 for sbj in "${SBJs[@]}"; do
     cd ${root_dir}/data/${sbj}/05_recon/
-    mkdir old_pipeline/
-    mv *full* old_pipeline/
-    mv *gROI* old_pipeline/
-    mv *compiled* old_pipeline/
+    #mkdir old_pipeline/
+    #mv *full* old_pipeline/
+    #mv *gROI* old_pipeline/
+    #mv *compiled* old_pipeline/
+    mv *tis* old_pipeline/
 done
 
 
